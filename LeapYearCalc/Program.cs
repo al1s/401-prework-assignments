7d5e8ccf754600a551d928d021f1e12fe9069a06
alstof <alstof@gmail.com>
2018-09-21 06:30:59 -0700

    Implement solution

--- LeapYearCalc/Program.cs
+++ LeapYearCalc/Program.cs
@@ -2,11 +2,19 @@
 
 namespace LeapYearCalc
 {
-    class Program
+  class Program
+  {
+    public static bool isLeap(string year)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      return (Int32.Parse(year) % 4 == 0 && Int32.Parse(year) % 100 != 0) ||
+              Int32.Parse(year) % 400 == 0;
     }
+    static void Main(string[] args)
+    {
+      Console.WriteLine($"Is 1997 leap: {isLeap("1997")}");
+      Console.WriteLine($"Is 1996 leap: {isLeap("1996")}");
+      Console.WriteLine($"Is 1900 leap: {isLeap("1900")}");
+      Console.WriteLine($"Is 2000 leap: {isLeap("2000")}");
+    }
+  }
 }
