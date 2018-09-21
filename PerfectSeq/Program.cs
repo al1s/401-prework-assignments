434dc4822ebf32cc3ae6334fa56018850aa80fc4
alstof <alstof@gmail.com>
2018-09-20 17:44:11 -0700

    Implement perfect sequence

--- PerfectSeq/Program.cs
+++ PerfectSeq/Program.cs
@@ -2,11 +2,32 @@
 
 namespace PerfectSeq
 {
-    class Program
+  class Program
+  {
+    public static string getPerfectSequence(int[] arr)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      string result = default(string);
+      int sum = default(int);
+      int product = 1;
+      foreach (int item in arr)
+      {
+        sum += item;
+        product *= item;
+      }
+      if (product == sum) result = "Yes";
+      else result = "No";
+      return result;
     }
+    static void Main(string[] args)
+    {
+      int[] arr = new int[] { 1, 2, 3 };
+      Console.WriteLine($"Is '{String.Join(" ", arr)}' perfect: {getPerfectSequence(arr)}");
+      arr = new int[] { 0, 0 };
+      Console.WriteLine($"Is '{String.Join(" ", arr)}' perfect: {getPerfectSequence(arr)}");
+      arr = new int[] { 2, 2 };
+      Console.WriteLine($"Is '{String.Join(" ", arr)}' perfect: {getPerfectSequence(arr)}");
+      arr = new int[] { 2, 2, 3 };
+      Console.WriteLine($"Is '{String.Join(" ", arr)}' perfect: {getPerfectSequence(arr)}");
+    }
+  }
 }
